5a7f834977a839e79f2dc135028f6d7367f804dd
paul <paul@paul-steele.com>
2022-12-30 17:09:56 -0500

    first commit

---
+++ Core/FerngillSupplyAndDemandMod.cs
@@ -0,0 +1,21 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+
+namespace fsd.core
+{
+    public class FerngillSupplyAndDemandMod : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += (sender, args) =>
+            {
+                if (!Context.IsWorldReady)
+                {
+                    return;
+                }
+
+                Monitor.Log(Game1.player.Name);
+            };
+        }
+    }
+}
