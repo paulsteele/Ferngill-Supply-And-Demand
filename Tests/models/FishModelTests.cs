7818b8fc59bc68e697f266f561c478a0fae6ef9d
Paul Steele <paul@paul-steele.com>
2024-05-01 19:15:53 -0400

    add seed / fish model tests

---
+++ Tests/models/FishModelTests.cs
@@ -0,0 +1,26 @@
+﻿using fse.core.models;
+
+namespace Tests.models;
+
+[TestFixture]
+public class FishModelTests
+{
+	[TestCase("spring", Seasons.Spring)]
+	[TestCase("summer", Seasons.Summer)]
+	[TestCase("fall", Seasons.Fall)]
+	[TestCase("winter", Seasons.Winter)]
+	[TestCase("spring summer", Seasons.Spring | Seasons.Summer)]
+	[TestCase("fall winter", Seasons.Fall | Seasons.Winter)]
+	[TestCase("spring summer fall winter", Seasons.Spring | Seasons.Summer | Seasons.Fall | Seasons.Winter)]
+	[TestCase("", 0)]
+	public void ShouldParseSeasonsAppropriately(string seasonString, Seasons expectedSeasons)
+	{
+		var fishModel = new FishModel("1", $"a/b/c/d/e/f/{seasonString}");
+			
+		Assert.Multiple(() => 
+		{
+			Assert.That(fishModel.ObjectId, Is.Not.Empty);
+			Assert.That(fishModel.Seasons, Is.EqualTo(expectedSeasons)); 
+		});
+	}
+}
---
+++ Tests/models/SeedModelTests.cs
@@ -0,0 +1,60 @@
+﻿using fse.core.models;
+using StardewValley;
+using StardewValley.GameData.Crops;
+
+namespace Tests.models;
+
+public class SeedModelTest
+{
+	private const string ObjectId = "TestId";
+	private CropData _cropEntry;
+
+	[SetUp]
+	public void Setup()
+	{
+		_cropEntry = new CropData
+		{
+			HarvestItemId = "TestCropId",
+			DaysInPhase = [1, 2, 3],
+		};
+	}
+
+	[Test]
+	public void Test_ObjectId()
+	{
+		var model = new SeedModel(ObjectId, _cropEntry);
+		Assert.That(model.ObjectId, Is.EqualTo(ObjectId));
+	}
+
+	[Test]
+	public void Test_CropId()
+	{
+		var model = new SeedModel(ObjectId, _cropEntry);
+		Assert.That(model.CropId, Is.EqualTo(_cropEntry.HarvestItemId));
+	}
+
+	[Test]
+	public void Test_DaysToGrow()
+	{
+		var model = new SeedModel(ObjectId, _cropEntry);
+		Assert.That(model.DaysToGrow, Is.EqualTo(6));
+	}
+
+	[TestCase(new[] {Season.Spring}, Seasons.Spring)]
+	[TestCase(new[] {Season.Spring, Season.Summer}, Seasons.Spring | Seasons.Summer)]
+	[TestCase(new[] {Season.Spring, Season.Fall}, Seasons.Spring | Seasons.Fall)]
+	[TestCase(new[] {Season.Spring, Season.Winter}, Seasons.Spring | Seasons.Winter)]
+	[TestCase(new[] {Season.Spring, Season.Summer, Season.Fall, Season.Winter}, Seasons.Spring | Seasons.Summer | Seasons.Fall | Seasons.Winter)]
+	public void Test_Seasons(Season[] seasonList, Seasons expectedSeasons)
+	{
+		_cropEntry = new CropData()
+		{
+			HarvestItemId = "TestCropId",
+			DaysInPhase = [1, 2, 3],
+			Seasons = seasonList.ToList(),
+		};
+		
+		var model = new SeedModel(ObjectId, _cropEntry);
+		Assert.That(model.Seasons, Is.EqualTo(expectedSeasons));
+	}
+}
