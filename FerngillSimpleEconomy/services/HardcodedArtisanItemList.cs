23dfca7a667d31b97120982a41367e2c031feade
Paul Steele <paul@paul-steele.com>
2024-09-29 21:12:29 -0400

    add sve, vapius, and phoenix hardcoded equivalences

--- FerngillSimpleEconomy/services/HardcodedArtisanItemList.cs
+++ FerngillSimpleEconomy/services/HardcodedArtisanItemList.cs
@@ -30,6 +30,33 @@ public static class HardcodedArtisanItemList
 				"445" => "698",
 				// raisins
 				"Raisins" => "398",
+				//blue egg
+				"6480.blueegg_BlueMayo" => "6480.blueegg_BlueEgg",
+				"6480.blueegg_GoldenMayo" => "928",
+				"6480.blueegg_OstrichMayo" => "289",
+				// Vapius Ewe
+				"Lumisteria.MtVapius_EweCheese" => "Lumisteria.MtVapius_EweMilk",
+				// Vapius Goose
+				"Lumisteria.MtVapius_GooseMayonnaise" => "Lumisteria.MtVapius_GooseEgg",
+				"Lumisteria.MtVapius_StellarGooseMayonnaise" => "Lumisteria.MtVapius_StellarGooseEgg",
+				// SVE Goose
+				"FlashShifter.StardewValleyExpandedCP_Goose_Mayonnaise" => "FlashShifter.StardewValleyExpandedCP_Goose_Egg",
+				// Vapius speckled
+				"Lumisteria.MtVapius_SpeckledFowlMayonnaise" => "Lumisteria.MtVapius_SpeckledFowlEgg",
+				"Lumisteria.MtVapius_SpeckledFowlVoidMayonnaise" => "Lumisteria.MtVapius_SpeckledFowlVoidEgg",
+				// Phoenix
+				"BrianOvaltine.Phoenixes_VoidPhoenixMayonnaise" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_AshenPhoenixMayonnaise" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_BluePhoenixMayonnaise" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_GhostPhoenixMayonnaise" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_GoldenPhoenixMayonnaise" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_GreenPhoenixMayonnaise" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_IcePhoenixMayonnaise" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_IridiumPhoenixMayonnaise" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_MagmaPhoenixMayonnaise" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_PrismaticPhoenixMayonnaise" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_RadioactivePhoenixMayonnaise" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_RedPhoenixMayonnaise" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
 				_ => null
 			};
 		}
--- FerngillSimpleEconomy/services/HardcodedEquivalentItemsList.cs
+++ FerngillSimpleEconomy/services/HardcodedEquivalentItemsList.cs
@@ -12,7 +12,39 @@ public static class HardcodedEquivalentItemsList
 				"182" => "176",
 				//milk
 				"186" => "184",
+				"Lumisteria.MtVapius_MilkExtraLarge" => "184",
 				"438" => "436",
+				"Lumisteria.MtVapius_GoatMilkExtraLarge" => "436",
+				//ewe Milk
+				"Lumisteria.MtVapius_EweMilkLarge" => "Lumisteria.MtVapius_EweMilk",
+				"Lumisteria.MtVapius_EweMilkExtraLarge" => "Lumisteria.MtVapius_EweMilk",
+				//Vapius goose egg
+				"Lumisteria.MtVapius_StellarGooseEgg" => "Lumisteria.MtVapius_GooseEgg",
+				// SVE goose egg
+				"FlashShifter.StardewValleyExpandedCP_Golden_Goose_Egg" => "FlashShifter.StardewValleyExpandedCP_Goose_Egg",
+				// Phoenix
+				"BrianOvaltine.Phoenixes_AshenPhoenixEgg" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_BluePhoenixEgg" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_GhostPhoenixEgg" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_GoldenPhoenixEgg" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_GreenPhoenixEgg" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_IcePhoenixEgg" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_IridiumPhoenixEgg" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_MagmaPhoenixEgg" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_PrismaticPhoenixEgg" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_RadioactivePhoenixEgg" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_VoidPhoenixEgg" => "BrianOvaltine.Phoenixes_RedPhoenixEgg",
+				"BrianOvaltine.Phoenixes_AshenPhoenixFeather" => "BrianOvaltine.Phoenixes_RedPhoenixFeather",
+				"BrianOvaltine.Phoenixes_BluePhoenixFeather" => "BrianOvaltine.Phoenixes_RedPhoenixFeather",
+				"BrianOvaltine.Phoenixes_GhostPhoenixFeather" => "BrianOvaltine.Phoenixes_RedPhoenixFeather",
+				"BrianOvaltine.Phoenixes_GoldenPhoenixFeather" => "BrianOvaltine.Phoenixes_RedPhoenixFeather",
+				"BrianOvaltine.Phoenixes_GreenPhoenixFeather" => "BrianOvaltine.Phoenixes_RedPhoenixFeather",
+				"BrianOvaltine.Phoenixes_IcePhoenixFeather" => "BrianOvaltine.Phoenixes_RedPhoenixFeather",
+				"BrianOvaltine.Phoenixes_IridiumPhoenixFeather" => "BrianOvaltine.Phoenixes_RedPhoenixFeather",
+				"BrianOvaltine.Phoenixes_MagmaPhoenixFeather" => "BrianOvaltine.Phoenixes_RedPhoenixFeather",
+				"BrianOvaltine.Phoenixes_PrismaticPhoenixFeather" => "BrianOvaltine.Phoenixes_RedPhoenixFeather",
+				"BrianOvaltine.Phoenixes_RadioactivePhoenixFeather" => "BrianOvaltine.Phoenixes_RedPhoenixFeather",
+				"BrianOvaltine.Phoenixes_VoidPhoenixFeather" => "BrianOvaltine.Phoenixes_RedPhoenixFeather",
 				_ => id
 			};
 		}
